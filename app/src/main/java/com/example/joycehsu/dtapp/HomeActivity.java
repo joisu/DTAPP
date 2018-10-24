610b97082684885fda944006d528ae6315018aec
joisu <joisuhsu@gmail.com>
2018-10-24 19:28:58 +1100

    news activity added

--- app/src/main/java/com/example/joycehsu/dtapp/HomeActivity.java
+++ app/src/main/java/com/example/joycehsu/dtapp/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.joycehsu.dtapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -12,17 +14,51 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HomeActivity extends BaseActivity {
+    String API_KEY = "4f3d80e4807f45bb96d5bce27eb2eb49";
+    String NEWS_SOURCE = "techcrunch";
+    ListView listNews;
+    ProgressBar loader;
+
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<HashMap<String, String>>();
+    static final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //inflate your activity layout here!
-        @SuppressLint("InflateParams")
+
+        //activity view
+    //    setContentView(R.layout.activity_home);
         View contentView = inflater.inflate(R.layout.activity_home, null, false);
         drawer.addView(contentView, 0);
+        listNews = (ListView) findViewById(R.id.listNews);
+        loader = (ProgressBar) findViewById(R.id.loader);
+        listNews.setEmptyView(loader);
+        if (Function.isNetworkAvailable(getApplicationContext())) {
+            DownloadNews newsTask = new DownloadNews();
+            newsTask.execute();
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+
         navigationView.setCheckedItem(R.id.nav_home);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,14 +68,74 @@ public class HomeActivity extends BaseActivity {
             }
         });
 
+
     }
 
+    class DownloadNews extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        protected String doInBackground(String... args) {
+            String xml = "";
+
+            String urlParameters = "";
+            xml = Function.excuteGet("https://newsapi.org/v1/articles?source=" + NEWS_SOURCE + "&sortBy=top&apiKey=" + API_KEY, urlParameters);
+            return xml;
+        }
+
+        @Override
+        protected void onPostExecute(String xml) {
+
+            if (xml.length() > 10) { // Just checking if not empty
+
+                try {
+                    JSONObject jsonResponse = new JSONObject(xml);
+                    JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR).toString());
+                        map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE).toString());
+                        map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION).toString());
+                        map.put(KEY_URL, jsonObject.optString(KEY_URL).toString());
+                        map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE).toString());
+                        map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT).toString());
+                        dataList.add(map);
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
+                }
+
+                ListNewsAdapter adapter = new ListNewsAdapter(HomeActivity.this, dataList);
+                listNews.setAdapter(adapter);
+
+                listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        Intent i = new Intent(HomeActivity.this, DetailsActivity.class);
+                        i.putExtra("url", dataList.get(+position).get(KEY_URL));
+                        startActivity(i);
+                    }
+                });
+
+            } else {
+                Toast.makeText(getApplicationContext(), "No news found", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -56,17 +152,5 @@ public class HomeActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onBackPressed() {
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                finishAffinity();
-            } else {
-                super.onBackPressed();
-            }
-        }
-    }
+
 }
